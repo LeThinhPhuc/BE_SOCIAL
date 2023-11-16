f3a2694ad3b81f9b50340db50695d00b9dfafad0
LeThinhPhuc <phuclethinh70@gmail.com>
2023-11-16 15:49:59 +0700

    c

--- src/main/java/com/example/social_be/controller/PostController.java
+++ src/main/java/com/example/social_be/controller/PostController.java
@@ -1,7 +1,11 @@
 package com.example.social_be.controller;
 
 import com.example.social_be.model.Post;
+import com.example.social_be.model.User;
+import com.example.social_be.repository.PostRepository;
+import com.example.social_be.repository.UserRepository;
 import com.example.social_be.service.PostService;
+import com.example.social_be.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -10,15 +14,27 @@ import java.util.List;
 @RestController
 @RequestMapping("/post")
 public class PostController {
-    public final PostService postService;
 
     @Autowired
-    public PostController(PostService postService){
-        this.postService=postService;
+    private UserRepository userRepository;
+    @Autowired
+    private PostRepository postRepository;
+    private final PostService postService;
+    private final UserService userService;
+
+    @Autowired
+    public PostController(PostService postService, UserService userService) {
+        this.postService = postService;
+        this.userService = userService;
     }
 
     @PostMapping("/add")
     public Post addPost(@RequestBody Post post){
+        User user1=userService.getOneUser(post.getUser().getId());
+//        User user1=userRepository.getById(id);
+
+        user1.setNumber_post(user1.getNumber_post()+1);
+        userService.updateUser(post.getUser().getId(),user1);
         return postService.addPost(post);
     }
 
@@ -26,5 +42,26 @@ public class PostController {
     public List<Post> listPost(@PathVariable("userId") String userId){
         return postService.listPost(userId);
     }
+
+    @PutMapping("/update/{id_post}")
+    public Post updatePost(@RequestBody Post post, @PathVariable("id_post") String id_post){
+        return postService.updatePost(id_post,post);
+    }
+
+    @DeleteMapping("/delete/{id_post}")
+    public Post deletePost(@PathVariable("id_post") String id_post){
+        User user1=userService.getOneUser(postRepository.getById(id_post).getUser().getId());
+//        User user1=userRepository.getById(id);
+
+        user1.setNumber_post(user1.getNumber_post()-1);
+        userService.updateUser(postRepository.getById(id_post).getUser().getId(),user1);
+        return postService.deletePost(id_post);
+    }
+
+    @GetMapping("/all")
+    public List<Post> getAllPost(){
+        return postService.getAllPost();
+    }
+
 
 }
